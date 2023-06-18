9006d82a51f3cf7a721e9f4d3da4387506a0e130
kidow <wcgo2ling@gmail.com>
2023-06-18 22:07:06 +0900

    typescript

---
+++ app/layout.tsx
@@ -0,0 +1,16 @@
+import './globals.css'
+
+export interface Props extends ReactProps {}
+
+export const metadata = {
+  title: '일간 ProductHunt',
+  description: '매일매일 ProductHunt 상위 5개 제품을 소개합니다.'
+}
+
+export default function RootLayout({ children }: Props) {
+  return (
+    <html lang="ko">
+      <body>{children}</body>
+    </html>
+  )
+}
---
+++ types/index.d.ts
@@ -0,0 +1,3 @@
+interface ReactProps {
+  children: ReactNode
+}
