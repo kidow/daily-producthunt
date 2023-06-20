38904a7f626e9faa0ad07dbec475f4bf4947f02f
kidow <wcgo2ling@gmail.com>
2023-06-20 17:28:01 +0900

    도메인 다시

--- app/api/redirect/tistory/route.ts
+++ app/api/redirect/tistory/route.ts
@@ -5,7 +5,7 @@ export async function GET(req: Request) {
   const url = new URL(req.url)
   const code = url.searchParams.get('code')
   const result = await request(
-    `https://www.tistory.com/oauth/access_token?client_id=${process.env.NEXT_PUBLIC_TISTORY_APP_ID}&client_secret=${process.env.NEXT_PUBLIC_TISTORY_SECRET_KEY}&redirect_uri=http://localhost:3000/api/redirect/tistory&code=${code}&grant_type=authorization_code`
+    `https://www.tistory.com/oauth/access_token?client_id=${process.env.NEXT_PUBLIC_TISTORY_APP_ID}&client_secret=${process.env.NEXT_PUBLIC_TISTORY_SECRET_KEY}&redirect_uri=https://dp.kidow.me/api/redirect/tistory&code=${code}&grant_type=authorization_code`
   )
 
   return NextResponse.json({ result })
