d61c706d24ae39adff531035d28ee57d8d82f168
kidow <wcgo2ling@gmail.com>
2023-06-26 15:46:26 +0900

    oauth access test

--- app/api/redirect/slack/route.ts
+++ app/api/redirect/slack/route.ts
@@ -6,22 +6,22 @@ import { cookies } from 'next/headers'
 export async function GET(req: Request) {
   const web = new WebClient(process.env.NEXT_PUBLIC_SLACK_TOKEN)
   const url = new URL(req.url)
-  const { access_token, authed_user } = await web.oauth.v2.access({
+  const result = await web.oauth.v2.access({
     client_id: process.env.NEXT_PUBLIC_SLACK_CLIENT_ID,
     client_secret: process.env.NEXT_PUBLIC_SLACK_CLIENT_SECRET,
     code: url.searchParams.get('code') as string,
     redirect_uri: 'https://daily-producthunt.kidow.me/api/redirect/slack'
   })
 
-  const bot = new WebClient(access_token)
+  const bot = new WebClient(result.access_token)
   const { user } = await bot.users.identity({
-    token: authed_user?.access_token
+    token: result.authed_user?.access_token
   })
   const { channels } = await bot.conversations.list({ types: 'im,mpim' })
-  const channelId = channels?.find((item) => item.user === authed_user?.id)?.id
+  const channelId = channels?.find(
+    (item) => item.user === result.authed_user?.id
+  )?.id
   if (!channelId) {
-    console.log('user', authed_user)
-    console.log('channels', channels)
     NextResponse.json({
       success: false,
       message:
@@ -38,25 +38,26 @@ export async function GET(req: Request) {
   if (data) {
     await supabase
       .from('connections')
-      .update({ slack_token: access_token, slack_channel_id: channelId })
+      .update({ slack_token: result.access_token, slack_channel_id: channelId })
       .eq('email', user?.email)
   } else {
     await supabase.from('connections').insert({
       email: user?.email || null,
-      slack_token: access_token,
+      slack_token: result.access_token,
       slack_channel_id: channelId
     })
   }
 
-  const result = await bot.chat.postMessage({
+  const { ok } = await bot.chat.postMessage({
     channel: channelId!,
     text: '통합이 완료되었습니다.'
   })
 
   return NextResponse.json({
-    success: result.ok,
-    message: result.ok
+    success: ok,
+    message: ok
       ? '통합이 완료되었습니다. 이 창을 닫아주세요.'
-      : '슬랙 연결에 실패했습니다. 문제가 계속 반복된다면 커뮤니티에 문의해주시기 바랍니다.'
+      : '슬랙 연결에 실패했습니다. 문제가 계속 반복된다면 커뮤니티에 문의해주시기 바랍니다.',
+    result
   })
 }
