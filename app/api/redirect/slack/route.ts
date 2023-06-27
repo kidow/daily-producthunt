45420a065b643d487ccafe71671d862b1d2b4a66
kidow <wcgo2ling@gmail.com>
2023-06-27 12:26:29 +0900

    모든 types, limit 1000

--- app/api/redirect/slack/route.ts
+++ app/api/redirect/slack/route.ts
@@ -14,66 +14,58 @@ export async function GET(req: Request) {
       code: url.searchParams.get('code') as string,
       redirect_uri: `${process.env.NEXT_PUBLIC_BASE_URL}/api/redirect/slack`
     })
-    const identity = await web.auth.test()
+
     const bot = new WebClient(result.access_token)
-    const message = await bot.chat.postMessage({
-      channel: '일간 ProductHunt',
-      text: '채널 이름으로 테스트'
+    const { user } = await bot.users.identity({
+      token: result.authed_user?.access_token
+    })
+    const { channels } = await bot.conversations.list({
+      types: 'public_channel,private_channel,mpim,im',
+      limit: 1000
+    })
+    const channelId = channels?.find(
+      (item) => item.user === result.authed_user?.id
+    )?.id
+    if (!channelId) {
+      console.log('channelId not found.')
+      return NextResponse.json({
+        success: false,
+        message:
+          '채널 id가 없습니다. 슬랙 연결에 실패했습니다. 문제가 계속 반복된다면 커뮤니티에 문의해주시기 바랍니다.'
+      })
+    }
+
+    const supabase = createRouteHandlerClient<Database>({ cookies })
+    const { data } = await supabase
+      .from('connections')
+      .select('*')
+      .match({
+        email: user?.email,
+        slack_token: result.access_token,
+        slack_channel_id: channelId
+      })
+      .single()
+    if (!data) {
+      await supabase.from('connections').insert({
+        email: user?.email || null,
+        slack_token: result.access_token,
+        slack_channel_id: channelId
+      })
+    }
+    const { ok } = await bot.chat.postMessage({
+      channel: channelId!,
+      text: data ? '이미 통합되어있습니다.' : '통합이 완료되었습니다.'
     })
 
-    return NextResponse.json({ result, identity, message })
-
-    // const bot = new WebClient(result.access_token)
-    // const { user } = await bot.users.identity({
-    //   token: result.authed_user?.access_token
-    // })
-    // const { channels } = await bot.conversations.list({
-    //   exclude_archived: true,
-    //   types: 'im'
-    // })
-    // const channelId = channels?.find(
-    //   (item) => item.user === result.authed_user?.id
-    // )?.id
-    // if (!channelId) {
-    //   console.log('channelId not found.')
-    //   NextResponse.json({
-    //     success: false,
-    //     message:
-    //       '슬랙 연결에 실패했습니다. 문제가 계속 반복된다면 커뮤니티에 문의해주시기 바랍니다.'
-    //   })
-    // }
-
-    // const supabase = createRouteHandlerClient<Database>({ cookies })
-    // const { data } = await supabase
-    //   .from('connections')
-    //   .select('*')
-    //   .match({
-    //     email: user?.email,
-    //     slack_token: result.access_token,
-    //     slack_channel_id: channelId
-    //   })
-    //   .single()
-    // if (!data) {
-    //   await supabase.from('connections').insert({
-    //     email: user?.email || null,
-    //     slack_token: result.access_token,
-    //     slack_channel_id: channelId
-    //   })
-    // }
-    // const { ok } = await bot.chat.postMessage({
-    //   channel: channelId!,
-    //   text: data ? '이미 통합되어있습니다.' : '통합이 완료되었습니다.'
-    // })
-
-    // return NextResponse.json({
-    //   success: ok,
-    //   message: ok
-    //     ? '통합이 완료되었습니다. 이 창을 닫아주세요.'
-    //     : '슬랙 연결에 실패했습니다. 문제가 계속 반복된다면 커뮤니티에 문의해주시기 바랍니다.',
-    //   result,
-    //   channels,
-    //   user
-    // })
+    return NextResponse.json({
+      success: ok,
+      message: ok
+        ? '통합이 완료되었습니다. 이 창을 닫아주세요.'
+        : '슬랙 연결에 실패했습니다. 문제가 계속 반복된다면 커뮤니티에 문의해주시기 바랍니다.',
+      result,
+      channels,
+      user
+    })
   } catch (err) {
     console.log(err)
     Sentry.captureException(err)
