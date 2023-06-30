3689b64a646d5ba9ddda4b799ecb81033e55ee07
kidow <wcgo2ling@gmail.com>
2023-06-30 16:57:39 +0900

    Input 컴포넌트 float 추가

--- app/api/redirect/slack/route.ts
+++ app/api/redirect/slack/route.ts
@@ -7,7 +7,7 @@ import * as Sentry from '@sentry/nextjs'
 export async function GET(req: Request) {
   const web = new WebClient(process.env.NEXT_PUBLIC_SLACK_TOKEN)
   const url = new URL(req.url)
-  const code = url.searchParams.get('code')
+  const code = url.searchParams.get('code') as string
   if (!code)
     return NextResponse.json({
       success: false,
@@ -17,7 +17,7 @@ export async function GET(req: Request) {
   const { incoming_webhook } = await web.oauth.v2.access({
     client_id: process.env.NEXT_PUBLIC_SLACK_CLIENT_ID,
     client_secret: process.env.NEXT_PUBLIC_SLACK_CLIENT_SECRET,
-    code: url.searchParams.get('code') as string,
+    code,
     redirect_uri: `${process.env.NEXT_PUBLIC_BASE_URL}/api/redirect/slack`
   })
 
