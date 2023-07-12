2abb9379c616993a4a627fe97a3ecd26c73863d9
kidow <wcgo2ling@gmail.com>
2023-07-12 17:51:28 +0900

    feat: 새 구독자 생성 시 웹훅으로 알림

--- app/api/connections/new/route.ts
+++ app/api/connections/new/route.ts
@@ -1,17 +1,19 @@
 import { NextResponse } from 'next/server'
 import { createRouteHandlerClient } from '@supabase/auth-helpers-nextjs'
 import { cookies } from 'next/headers'
-import { WebClient } from '@slack/web-api'
 
 export async function POST(req: Request) {
   const supabase = createRouteHandlerClient<Database>({ cookies })
   const { count } = await supabase
     .from('connections')
     .select('id', { count: 'exact' })
-  const web = new WebClient(process.env.NEXT_PUBLIC_SLACK_TOKEN)
-  await web.chat.postMessage({
-    channel: 'D05D1L17KQT',
-    text: `새 구독자가 생겼습니다. 총 ${count || 0}명`
+
+  await fetch(process.env.NEXT_PUBLIC_SLACK_WEBHOOK_URL, {
+    method: 'POST',
+    headers: new Headers({ 'Content-Type': 'application/json' }),
+    body: JSON.stringify({
+      text: `새 구독자가 생겼습니다. 총 ${count || 0}명`
+    })
   })
   return NextResponse.json({ success: true })
 }
