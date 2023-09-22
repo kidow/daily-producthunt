4744f677db421c9dc73729477868768dfa5d6c22
kidow <wcgo2ling@gmail.com>
2023-09-22 10:29:16 +0900

    gtm

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import { Backdrop, Toast } from 'containers'
 import './globals.css'
 import type { Metadata } from 'next'
+import Script from 'next/script'
 
 export interface Props extends ReactProps {}
 
@@ -46,6 +47,17 @@ export default function RootLayout({ children }: Props) {
   return (
     <html lang="ko">
       <body>
+        <Script
+          id="gtm"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-WVMQ8J6T');`
+          }}
+        />
         {children}
         <Backdrop />
         <Toast />
